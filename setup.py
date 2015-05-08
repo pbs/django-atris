4e6fd53f54738ed934fd944f638ebd7276f5f431
Bogdan Pop <bogdan.pop@3pillarglobal.com>
2015-05-08 16:55:00 +0300

    Added setup and manifest.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import history_logging
+
+tests_require = ["Django>=1.4", "webtest==2.0.6", "django-webtest==1.7"]
+try:
+    from unittest import skipUnless
+except ImportError:  # Python 2.6 compatibility
+    tests_require.append("unittest2")
+
+setup(
+    name='django-history-logging',
+    version=history_logging.__version__,
+    description='History logging.',
+    long_description='History logging.',
+    author='Bogdan Andrei Pop',
+    url='https://github.com/bogdanpop/django-history-logging',
+    packages=["history_logging"],
+    tests_require=tests_require,
+    include_package_data=True,
+    test_suite='runtests.main',
+)

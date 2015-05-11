c8b608f93d11f846d0b0e47b785acea72dcf3504
Bogdan Pop <bogdan.pop@3pillarglobal.com>
2015-05-11 18:27:02 +0300

    Stable initial version. Added several functionalities.

--- history_logging/models.py
+++ history_logging/models.py
@@ -1,9 +1,11 @@
 import threading
+import json
+
 from django.contrib.postgres.fields import HStoreField
 from django.db import models
-from django.utils import importlib
+from django.utils.importlib import import_module
 from django.utils.timezone import now
-# from django.contrib.postgres.fields import HStoreField
+from django.core import serializers
 
 
 class HistoryLogging(object):
@@ -21,10 +23,12 @@ class HistoryLogging(object):
             except AttributeError:
                 return None
 
+    def contribute_to_class(self, cls, name):
+        self.manager_name = name
+        self.module = cls.__module__
+        models.signals.class_prepared.connect(self.finalize, sender=cls)
+
     def finalize(self, sender, **kwargs):
-        history_model = self.create_history_model(sender)
-        module = importlib.import_module(self.module)
-        setattr(module, history_model.__name__, history_model)
 
         # The HistoricalRecords object will be discarded,
         # so the signal handlers can't use weak references.
@@ -45,13 +49,28 @@ class HistoryLogging(object):
     def create_historical_record(self, instance, history_type):
         history_date = getattr(instance, '_history_date', now())
         history_user = self.get_history_user(instance)
-        data = instance.__dict__
-        historical_record = HistoricalRecord.objects.create(
+        data = serializers.serialize('json', [instance])
+        struct = json.loads(data)
+        data = dict(
+            (key, unicode(val)) for key, val in struct[0]['fields'].items())
+
+        HistoricalRecord.objects.create(
+            model_id=instance.id,
+            history_object_qualified_path=fullname(instance),
             history_date=history_date,
             history_type=history_type,
             history_user=history_user,
             data=data
         )
+
+
+def fullname(o):
+    """
+    Return the full qualified name of a given class.
+    :rtype : String
+    :param o: Class object.
+    """
+    return u"{0}.{1}".format(o.__module__, o.__class__.__name__)
 
 
 class HistoricalRecord(models.Model):
@@ -67,4 +86,69 @@ class HistoricalRecord(models.Model):
         ('-', 'Deleted'),
     ))
     data = HStoreField()
+
+    def __unicode__(self):
+        return '{0} {1} id={2}'.format(
+            self.get_history_type_display(),
+            self.history_object_qualified_path,
+            self.model_id
+        )
+
+    def get_history_object_class(self):
+        """
+        Return the class object from the history object dotted path.
+
+        :raises: ValueError if the class could not be loaded
+        """
+        try:
+            module_path, class_name = self.history_object_qualified_path.rsplit(
+                ".", 1)
+        except ValueError:
+            raise ValueError("Invalid class dotted path: %s"
+                             % self.history_object_qual_path)
+
+        try:
+            module = import_module(module_path)
+        except ImportError:
+            raise ValueError(
+                "Failed to import module: %s" % module_path)
+
+        class_object = getattr(module, class_name, None)
+        if class_object is None:
+            raise ValueError(
+                "No %s class found in module %s" % (class_name, module_path))
+
+        return class_object
+
+    def get_superficial_diff_string(self):
+        object_snapshot = self.get_current_snapshot()
+        diff_string = u'{} '.format(object_snapshot.get_history_type_display())
+
+        if u'Updated' not in diff_string:
+            return diff_string
+
+        previous_version = self.get_previous_version_snapshot()
+
+        diff_string += ', '.join(
+            [
+                '{}'.format(attr.replace('_', ' ').capitalize())
+                for (attr, val) in object_snapshot.data.items()
+                if (attr, val) not in previous_version.data.items()
+                and not attr.startswith(('history_', '_'))
+            ]
+        )
+        return diff_string
+
+    def get_current_snapshot(self):
+        return self
+
+    def get_previous_version_snapshot(self):
+        return HistoricalRecord.objects.filter(
+            model_id=self.model_id,
+            history_object_qualified_path=self.history_object_qualified_path,
+            id__lt=self.id
+        ).order_by('-id').first()
+
+    def get_history_object_class_name(self):
+        return self.history_object_qual_path.split('.')[-1]
 

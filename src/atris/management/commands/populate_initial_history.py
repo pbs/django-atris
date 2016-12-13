51f7ec545cbd4a2e3b51714c243ab0d684a6376c
Cristina Botez <cristina.botez@3pillarglobal.com>
2016-12-13 12:44:21 +0200

    Added logs in populate_initial_history. Increased verbosity for test output.

--- src/atris/management/commands/populate_initial_history.py
+++ src/atris/management/commands/populate_initial_history.py
@@ -16,7 +16,7 @@ str = str if six.PY2 else str
 
 
 class Command(BaseCommand):
-    EXISTING_HISTORY_FOUND = "Existing history found, skipping model"
+    EXISTING_HISTORY_FOUND = 'Existing history found, skipping model'
 
     SELECT_BATCH_SIZE = 1000
     CREATE_BATCH_SIZE = 1000
@@ -37,13 +37,14 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        for (model, model_specific_info) in registered_models.items():
+        for model, model_specific_info in registered_models.items():
             if HistoricalRecord.objects.by_model(model).exists():
-                self.stderr.write("{msg} {model}\n".format(
+                self.stderr.write('{msg} {model}\n'.format(
                     msg=self.EXISTING_HISTORY_FOUND,
                     model=model,
                 ))
                 continue
+            self.stdout.write('Initializing hsitory for {}\n'.format(model))
             additional_data_param_name = model_specific_info[
                 'additional_data_param_name']
             additional_data_field = getattr(model, additional_data_param_name,
@@ -53,7 +54,8 @@ class Command(BaseCommand):
             excluded_fields = getattr(model, excluded_fields_param_name, [])
             create_history_for_model = ModelHistoryCreator(
                 model, additional_data_field, excluded_fields,
-                options['select_batch_size'], options['create_batch_size']
+                options['select_batch_size'], options['create_batch_size'],
+                self.stdout
             )
             create_history_for_model()
 
@@ -61,20 +63,27 @@ class Command(BaseCommand):
 class ModelHistoryCreator(object):
 
     def __init__(self, model, additional_data_field, excluded_fields,
-                 select_batch_size, create_batch_size):
+                 select_batch_size, create_batch_size, output):
         self.model = model
         self.additional_data_field = additional_data_field
         self.excluded_fields = excluded_fields
         self.select_batch_size = select_batch_size
         self.create_batch_size = create_batch_size
+        self.output = output
 
     def __call__(self):
         objects = self.model.objects
-        number_of_batches = ceil(objects.count() / self.select_batch_size)
-        for multiplicity in range(int(number_of_batches)):
+        number_of_batches = int(ceil(objects.count() / self.select_batch_size))
+        self.output.write(
+            'Processing data in {} batches of {} target objects.\n'
+            .format(number_of_batches, self.select_batch_size)
+        )
+        for multiplicity in range(number_of_batches):
             start = self.select_batch_size * multiplicity
             end = start + self.select_batch_size
             self.create_history_for_objects(objects.all()[start:end])
+            self.output.write('Finished batch #{} of {}.\n'
+                              .format(multiplicity + 1, number_of_batches))
 
     def create_history_for_objects(self, objects):
         historical_instances = []
@@ -107,7 +116,7 @@ class ModelHistoryCreator(object):
                 try:
                     value = getattr(instance, field.attname)
                 except AttributeError:
-                    self.stdout.write(('Field "{}" is invalid.'.format(key)))
+                    self.output.write(('Field "{}" is invalid.'.format(key)))
                 else:
                     if value is not None:
                         value = str(value)

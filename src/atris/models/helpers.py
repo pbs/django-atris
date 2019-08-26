09d302657bdc1ec9be551b1859c22112d47941be
David Veza <david.veza@3pillarglobal.com>
2019-08-26 17:55:45 +0300

    Order m2m relations when serializing instance data. Also safe-check at compare time to prevent new fake events after sorting.

--- src/atris/models/helpers.py
+++ src/atris/models/helpers.py
@@ -1,8 +1,11 @@
+import re
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import router
 
 
-def get_diff_fields(model, data, previous_data, excluded_fields_names):
+def get_diff_fields(model, data, previous_data, excluded_fields_names,
+                    check_fake=True):
     """
     Returns the fields of `data` for which the values differ in
     `previous_data`. The fields that are given in `excluded_fields_names` are
@@ -11,9 +14,34 @@ def get_diff_fields(model, data, previous_data, excluded_fields_names):
     """
     if not previous_data:
         return []
-    return [model._meta.get_field(f).name
-            for f, v in data.items()
-            if f not in excluded_fields_names and previous_data.get(f) != v]
+    diff_fields = [
+        model._meta.get_field(f).name for f, v in data.items()
+        if f not in excluded_fields_names and previous_data.get(f) != v
+    ]
+    if check_fake and diff_is_fake(data, previous_data, diff_fields):
+        # Takes into account that in atris < 1.4
+        # m2m relation ids were unordered,
+        # which often yielded false positives
+        return []
+    return diff_fields
+
+
+def diff_is_fake(data, prev_data, diff_fields):
+    """
+    If all the diff_fields have values like '123, 456' vs '456, 123'
+    it means it's a false diff.
+    """
+    for field_name in diff_fields:
+        value = data[field_name]
+        id_list = re.match(r'^(\d+,\s)+\d+$', value)
+        if not id_list:
+            return False
+        else:
+            old_set = set(prev_data[field_name].split(', '))
+            curr_set = set(data[field_name].split(', '))
+            if old_set != curr_set:
+                return False
+    return True
 
 
 def get_instance_field_data(instance):
@@ -34,7 +62,7 @@ def get_instance_field_data(instance):
             value = None
         if field.many_to_many or field.one_to_many:
             ids = from_writable_db(value).values_list('pk', flat=True)
-            data[name] = ', '.join([str(e) for e in ids])
+            data[name] = ', '.join([str(e) for e in ids.order_by('pk')])
         elif field.one_to_one and not field.concrete:
             data[name] = str(value.pk) if value is not None else None
         else:
